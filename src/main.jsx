900370715f5ed84e4c7d338e3d4295af76cf4d40
Dejanrobi <dejanrobi93@gmail.com>
2024-04-18 08:28:18 +0300

    ready to deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { CompanyContextProvider } from './context/CompanyContext.jsx'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
+if (import.meta.env.NODE_ENV  === 'production') disableReactDevTools()
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
